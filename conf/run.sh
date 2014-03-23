37b9719c13c4cc14cc6cd66f17b5b1f1c435aff2
HaLara <Henk.Laracker@planonsoftware.com>
2014-03-23 22:57:22 +0100

    .

--- conf/run.sh
+++ conf/run.sh
@@ -6,9 +6,9 @@ MAINCLASS="nl.planon.heracles.client.application.UpgradeTool"
 
 MODE="-upgradeAll -batch"
 
-CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
-
 cd $OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/
+
+CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
 
 java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log 
 
