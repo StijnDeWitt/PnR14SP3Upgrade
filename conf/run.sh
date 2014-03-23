68e4da2ff1adbc8d0b99e24927eb375d011cf892
HaLara <Henk.Laracker@planonsoftware.com>
2014-03-23 22:02:01 +0100

    fixed some small issues

--- conf/run.sh
+++ conf/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-JAVAOPTIONS=-Djava.security.auth.login.config=.\auth.conf
+JAVAOPTIONS=-Djava.security.auth.login.config=$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/auth.conf
 
 MAINCLASS="nl.planon.heracles.client.application.UpgradeTool"
 
-MODE=-upgradeAll
+MODE="-upgradeAll -batch"
 
 CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
 
-java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE > %$OPENSHIFT_PLN_DIR/logs/upgrade-swing.log 
+java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log 
 
