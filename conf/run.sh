3ef2cc563b69e1bb88d8910f307d387a9482725b
HaLara <Henk.Laracker@planonsoftware.com>
2014-03-24 08:45:57 +0100

    .

--- conf/run.sh
+++ conf/run.sh
@@ -10,5 +10,5 @@ cd $OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/
 
 CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
 
-java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log 
+java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log &
 
