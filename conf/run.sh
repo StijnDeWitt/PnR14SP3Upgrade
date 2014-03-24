474f845c470221a2ae4e7ea273e57761eb59d7c8
HaLara <Henk.Laracker@planonsoftware.com>
2014-03-24 16:14:22 +0100

    test script background

--- conf/run.sh
+++ conf/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source $OPENSHIFT_CARTRIDGE_SDK_BASH
+
 JAVAOPTIONS="-Djava.security.auth.login.config=$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/auth.conf"
 
 MAINCLASS="nl.planon.heracles.client.application.UpgradeTool"
@@ -10,4 +12,8 @@ cd $OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/
 
 CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
 
-java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE 2> $OPENSHIFT_PLN_DIR/logs/upgrade-swing-errors.log > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log
+sleep 1000
+
+#java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE 2> $OPENSHIFT_PLN_DIR/logs/upgrade-swing-errors.log > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log &
+
+client_result "   Update(Swing) run.sh executed"
