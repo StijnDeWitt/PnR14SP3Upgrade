b281c89a28bf646971e5ebcb4552e6cf2ddc5331
HaLara <Henk.Laracker@planonsoftware.com>
2014-03-27 15:48:04 +0100

    wait added for http

--- conf/run.sh
+++ conf/run.sh
@@ -8,7 +8,7 @@ function ishttpup() {
     count=0
     while [ ${count} -lt 40 ]
     do
-        if /usr/sbin/lsof -P -n -i "@${OPENSHIFT_PLN_IP}:${OPENSHIFT_PLN_HTTP_PORT}" | grep "(LISTEN)" > /dev/null; then
+        if /usr/sbin/lsof -P -n -i "@${OPENSHIFT_PLN_IP}:${OOPENSHIFT_PLN_HTTP_PORT_JBOSS}" | grep "(LISTEN)" > /dev/null; then
             echo "Found ${OPENSHIFT_PLN_IP}:${OPENSHIFT_PLN_HTTP_PORT} listening port"
             return 0
         fi
@@ -21,6 +21,8 @@ function ishttpup() {
 
 function startUpgrade(){
 
+ishttpup
+
 JAVAOPTIONS="-Djava.security.auth.login.config=$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/auth.conf"
 
 MAINCLASS="nl.planon.heracles.client.application.UpgradeTool"
@@ -31,15 +33,15 @@ cd $OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/
 
 CLASSPATH=$(JARS=("$LIB"/$OPENSHIFT_PLN_DIR/PlanonProCenter/ProCenterClient/*.jar); IFS=:; echo "${JARS[*]}")
 
-nohup java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE 2> $OPENSHIFT_PLN_DIR/logs/upgrade-swing-errors.log > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log &
+java -cp $CLASSPATH $JAVAOPTIONS $MAINCLASS $MODE 2> $OPENSHIFT_PLN_DIR/logs/upgrade-swing-errors.log > $OPENSHIFT_PLN_DIR/logs/upgrade-swing.log
 
 client_result "   Update(Swing) run.sh executed"
 
 return 0
+}
 
 case "$1" in
-  start)     start ;;
-  ishttpup)  ishttpup ;;
-  *)         exit 0
+  startUpgrade) startUpgrade ;;
+  ishttpup)     ishttpup ;;	
+  *)            exit 0
 esac
-}
